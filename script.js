6b94e60200ab4487ff20d59c2f5d379c705f8a9f
ekaaptrnnn <eka4ptrn_.uns.ac.id@student.uns.ac.id>
2026-07-30 23:27:14 +0700

    add link

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const LOMBA_DATA = {
         icon: '🎸',
         desc: 'Tampilkan performa terbaik band-mu di atas panggung Edufest 2026.',
         poster: 'poster-band.png',
-        handbook: 'https://drive.google.com/file/d/1r2O1_8KBMXrnLZGsg4MOUQmhH3ijhIXE/view?usp=drive_link',
+        handbook: 'https://drive.google.com/file/d/1r2O1_8KBMXrnLZGsg4MOUQmhH3ijhIXE/view?usp=sharing',
         twibbon: 'https://uns.id/TwibbonPesertaEdufest2026',
+        captionTwibbon: 'https://docs.google.com/document/d/1ye4zzjLJoAkknrJWbWTz_2bVsxm2Q03XJSYLtJ-Fwm4/edit?usp=sharing', // Sesuaikan link caption jika ada
         berkas: 'https://drive.google.com/drive/folders/1i7h4sSKd2YocMGT1zD2eqqoYOZB4WOEI?usp=drive_link',
         pendaftaran: 'https://docs.google.com/forms/d/e/1FAIpQLSeVWBjze6aAkp3yG7ErEJ42B79xbBAcaqSzD240r5gg4tm3bg/viewform?usp=publish-editor',
         pengumpulanKarya: 'https://uns.id/SubmitKaryaEdufest2026',
@@ -14,9 +15,10 @@ const LOMBA_DATA = {
         icon: '🎮',
         desc: 'Adu strategi dan skill bareng tim e-sport terbaik kampus.',
         poster: 'poster-esport.jpg',
-        handbook: 'MASUKKAN_LINK_HANDBOOK_ESPORT_DISINI',
+        handbook: 'https://drive.google.com/file/d/1WT8PJrenISHF721po9mZ-Xia4b2KNedA/view?usp=drivesdk',
         twibbon: 'https://uns.id/TwibbonPesertaEdufest2026',
-        berkas: 'MASUKKAN_LINK_BERKAS_ESPORT_DISINI',
+        captionTwibbon: 'https://docs.google.com/document/d/17CcVrTMRdipI5tFDmT5zJUx9FzkSYqB2/edit?usp=drive_link&ouid=108089471838212221847&rtpof=true&sd=true', // Sesuaikan link caption jika ada
+        berkas: 'https://drive.google.com/drive/folders/1AoAAGT1eKLFtSomuDzwnxtM99Q3OfYNW',
         pendaftaran: 'https://forms.gle/gqCMSB1PSfaW9BnAA',
         pengumpulanKarya: null,
         contactPerson: 'https://wa.me/6285727326474'
@@ -24,9 +26,10 @@ const LOMBA_DATA = {
     'Futsal Competition': {
         icon: '⚽',
         desc: 'Kompak, kompetitif, dan penuh semangat sportivitas di lapangan.',
-        poster: 'poster-futsal.png',
-        handbook: 'MASUKKAN_LINK_HANDBOOK_FUTSAL_DISINI',
+        poster: 'poster-futsal.jpg',
+        handbook: 'https://drive.google.com/file/d/1WT8PJrenISHF721po9mZ-Xia4b2KNedA/view?usp=drivesdk',
         twibbon: 'https://uns.id/TwibbonPesertaEdufest2026',
+        captionTwibbon: 'https://docs.google.com/document/d/1g8L5ZNh20b46LKNJx8R9vrj47zsHfWMZFCV_Tsq8iz8/edit?usp=sharing', // Sesuaikan link caption jika ada
         berkas: 'https://docs.google.com/document/d/1QT8fAeAD3uDJNVT0v8ik20bGSm4w0oXg/edit?usp=drive_link&ouid=108089471838212221847&rtpof=true&sd=true',
         pendaftaran: 'https://docs.google.com/forms/d/e/1FAIpQLSeITvohk8OzVSDhu_P03fyoXMMgbyqyEKUF52KJlSNnob6lGQ/viewform?usp=header',
         pengumpulanKarya: null,
@@ -59,7 +62,16 @@ function openDetail(namaLomba) {
     document.getElementById('menu-pendaftaran').href = data.pendaftaran;
     document.getElementById('menu-contact-person').href = data.contactPerson;
 
-    // 4. Menu "Pengumpulan Karya" hanya tampil kalau lomba punya link-nya (khusus Band)
+    // 4. Menu "Caption Twibbon" diatur link dan kondisinya
+    const menuCaptionTwibbon = document.getElementById('menu-caption-twibbon');
+    if (data.captionTwibbon) {
+        menuCaptionTwibbon.href = data.captionTwibbon;
+        menuCaptionTwibbon.style.display = 'flex';
+    } else {
+        menuCaptionTwibbon.style.display = 'none';
+    }
+
+    // 5. Menu "Pengumpulan Karya" hanya tampil kalau lomba punya link-nya (khusus Band)
     const menuPengumpulanKarya = document.getElementById('menu-pengumpulan-karya');
     if (data.pengumpulanKarya) {
         menuPengumpulanKarya.href = data.pengumpulanKarya;
@@ -68,7 +80,7 @@ function openDetail(namaLomba) {
         menuPengumpulanKarya.style.display = 'none';
     }
 
-    // 5. Pindah halaman & scroll ke atas
+    // 6. Pindah halaman & scroll ke atas
     document.getElementById('home-page').classList.remove('active');
     document.getElementById('detail-page').classList.add('active');
     window.scrollTo({ top: 0, behavior: 'smooth' });
